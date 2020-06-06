60858049500a751442b4a08242113c58f23e59c1
Adam Wong <adamwthedev@gmail.com>
2020-06-06 00:35:14 -0700

    reformatted index.html file

---
+++ src/pages/about.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Link } from 'gatsby'
+import Layout from '../components/layout'
+import Styled from 'styled-components'
+
+const AboutDiv = Styled.div`
+    text-align: center;
+`
+
+const About = () => {
+    return (
+        <Layout>
+            <AboutDiv>
+                <h1>About Page</h1>
+            </AboutDiv>
+            <Link to='/'>Go back to home page</Link>
+        </Layout>
+    )
+}
+
+export default About;

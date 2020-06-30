81aa2379b5b3221416c779fa5a0308d93a8b4fd5
Adam Wong <adamwthedev@gmail.com>
2020-06-30 15:01:09 -0700

    added recipes page

--- src/pages/recipes.js
+++ src/pages/recipes.js
@@ -2,21 +2,21 @@ import React from "react"
 import Layout from "../components/layout"
 import Styled from "styled-components"
 
-const DiscoveredDiv = Styled.div`
+const RecipesDiv = Styled.div`
     min-height: 80vh;
     margin-bottom: 5vh;
     text-align: center;
     color: white;
 `
 
-const Discovered = () => {
+const Recipes = () => {
     return (
         <Layout>
-            <DiscoveredDiv>
-                <h1>Discovered Page</h1>
-            </DiscoveredDiv>
+            <RecipesDiv>
+                <h1>Recipes Page</h1>
+            </RecipesDiv>
         </Layout>
     )
 }
 
-export default Discovered
+export default Recipes
